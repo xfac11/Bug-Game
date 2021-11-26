3d77f075dc064fd77ff4acc2dc4cd15c2d6eb70a
Filip Karlsson <xfac115@gmail.com>
2021-11-26 23:41:43 +0100

    New Wavehandler script
    Wavehandler has a spawnerprefab that it will spawn at random set location. It also has an difficulty that will change the behaviour of the enemies.
    A wavenumber to indicate which wave we are currently on, and a TimeBetweenWaves that is the time between each wave. Before that starts ticking the OnWaveFinished will be invoked and can update the UI and stuff. When is has reached 0 the new wave will begin and spawner object are instantiated and the OnNewWave will be invoked to update UI and stuff.

---
+++ Assets/Scripts/WaveHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveHandler : MonoBehaviour
+{
+    public Action OnNewWave;
+    public Action OnWaveFinished;
+
+    [SerializeField] private GameObject SpawnerPrefab;
+    [SerializeField] private int Difficulty;
+    [SerializeField] private int WaveNumber;
+    [SerializeField] private int TimeBetweenWaves;
+}
